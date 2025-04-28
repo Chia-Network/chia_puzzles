5a6060f79f409c22e38f5eb77af9b0d24b978a65
Rigidity <me@rigidnetwork.com>
2025-04-28 17:04:05 -0400

    Fix imports

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 mod tests {
-    use crate::programs::{CAT_PUZZLE, SINGLETON_TOP_LAYER};
+    use crate::programs::{CAT_PUZZLE_V2, SINGLETON_TOP_LAYER};
     #[test]
     fn puzzle_hashes() {
-        assert!(CAT_PUZZLE.len() == 1672);
+        assert!(CAT_PUZZLE_V2.len() == 1672);
         assert!(SINGLETON_TOP_LAYER.len() == 1168);
     }
 }
