0487573105ba1995883857e5b56bab28568a60c7
Rigidity <me@rigidnetwork.com>
2025-06-07 22:29:10 -0400

    Fix test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 mod programs;
-mod tests;
 
 pub use programs::*;
+
+#[cfg(test)]
+mod tests {
+    use crate::programs::{CAT_V2, SINGLETON_TOP_LAYER_V2};
+    #[test]
+    fn puzzle_hashes() {
+        assert_eq!(CAT_V2.len(), 1672);
+        assert_eq!(SINGLETON_TOP_LAYER_V2.len(), 967);
+    }
+}
